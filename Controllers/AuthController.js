b3cb59d2b99e120739c29319b0dd7315092afb54
JIMSHADTP <108132159+JIMSHADTP@users.noreply.github.com>
2023-03-02 10:41:32 +0530

    second commit

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,16 +1,16 @@
 import userModel from '../Models/userModel.js'
-
+import bcrypt from 'bcrypt'
 
 
 export const registerUser = async (req, res) => {
     try {
         const { username, firstname, lastname, password } = req.body;
-        const newuser = new UserModel({ username, firstname, lastname, password })
+        const newuser = new userModel({ username, firstname, lastname, password })
         await newuser.save()
         res.status(200).json(newuser)
     } catch (error) {
         res.status(500).json(error)
-        console.log("error aayi")
+        console.log(error)
 
     }
 }
