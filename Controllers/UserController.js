b6212b8d7ccb13db97fa4050000c7cedbf81c3d2
JIMSHADTP <108132159+JIMSHADTP@users.noreply.github.com>
2023-03-03 15:23:49 +0530

    fourth commit

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,4 +1,5 @@
 import userModel from '../Models/userModel.js'
+import bcrypt from 'bcrypt'
 
 //get a User
 
@@ -11,8 +12,53 @@ export const getUser = async (req,res) => {
         if(user){
             const {password ,...otherDetails} = user._doc;
             res.status(200).json(otherDetails)
+        }else{
+            res.status(400).json("No such user exist")
         }
     } catch (error) {
         
     }
 }
+
+//update a user
+
+export const updateUser = async (req,res) => {
+    const id = req.params.id
+    try {
+        const {currentUserId,currentUserAdminStatus,password} = req.body;
+
+        if(id===currentUserId || currentUserAdminStatus) {
+            if(password){
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(password,salt)
+            }
+            const user = await userModel.findByIdAndUpdate(id,req.body,{new:true})
+            res.status(200).json(user)
+        }else {
+            res.status(403).json("Access Denied! you can only update your own profile")
+        }
+   
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+export const deleteUser = async (req, res) => {
+  const id = req.params.id
+  const {currentUserId,currentUserAdminStatus} = req.body;
+      try {
+        if(currentUserId === id || currentUserAdminStatus){
+            await userModel.findByIdAndDelete(id);
+            res.status(200).json("User deleted successfully");
+        }
+        
+      } catch (error) {
+        res.status(500).json(error);
+      }
+
+  };
+  
+  //follow a user
+
+  export const followUser = async (req,res) =>{
+     
+  }
